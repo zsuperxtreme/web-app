d35ab9110e4181473d206e472c76c64e3a00ee91
Zainal Abdul Kahar <zsuperxtreme@gmail.com>
2026-03-31 02:36:32 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -18,21 +18,20 @@ function App() {
         <div>
           <h1>Get started</h1>
 
-
-<div className="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Testing</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+          <div className="card bg-base-100 w-96 shadow-sm">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoe</h2>
+              <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
           
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
